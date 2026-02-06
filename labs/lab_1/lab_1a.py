c8721121747755a3d9b56e44dbbd5a457b40711e
kuandai <kuandai@kuandai.me>
2026-02-05 19:11:31 -0800

    feat(lab 1a): Add personal introduction to lab 1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -12,5 +12,11 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
 
+    print("""Hello everyone, my name is Kuan Dai and I am a high school sophomore located in Washington. I enjoy small scale electronics projects (ESPHome trinkets, mesh RF, and a remote control RC car I built), maintaining and expanding my homelab infrastructure, and programming in my free time! I've also been playing with FPV lately, although only at the simulator level so far. I have experience in C++ and Java, and am currently dabbling in some Rust.
+
+I find it to be quite relaxing and empowering to deploy my own selfhosted services, rather than using large corporate solutions. I maintain a Nextcloud instance for my family and friends, as well as email, Jellyfin, and an OIDC provider to string it all together. I'm currently working on a PKI-based authentication system in C++ for my Minecraft clone.
+
+Looking forwards to getting to know everybody better.""")
+
 if __name__ == "__main__":
     main()
